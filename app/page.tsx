f2a1fbd6e4e94c12ba26533d5bdce665a0605864
Angelo Ilardi <ange448@gmail.com>
2023-07-07 00:38:02 +0100

    another fix for the video width

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,18 @@ export default function Home() {
     return (
       <div className="h-screen flex items-center justify-center pl-1 pr-1 m-auto text-center overflow-x-hidden text-white font-bold">
         <div className="absolute top-0 left-0 w-full h-screen overflow-hidden">
-                <video
-                    className='!min-w-full !min-h-full h-0 max-w-none'
+          <video
+            className="!min-w-full !min-h-full h-0 max-w-none object-cover"
             src="./video.mp4"
             muted
             autoPlay
             loop
-                ></video>
-                <div className='h-screen w-screen absolute top-0 left-0 bg-blue-900 z-10 opacity-60'></div>
-            </div>
-            <div className="z-20">
-                <h1 className='text-9xl'>LONDONANDO</h1>
-            </div>
+          ></video>
+          <div className="h-screen w-screen absolute top-0 left-0 bg-blue-900 z-10 opacity-70"></div>
+        </div>
+        <div className="z-20">
+          <h1 className="text-5xl md:text-6xl lg:text-8xl">LONDONANDO</h1>
+        </div>
       </div>
     );
 }
