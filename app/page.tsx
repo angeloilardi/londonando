8730e7e976e0e42f4446da537df9d24d7d893f4d
Angelo Ilardi <ange448@gmail.com>
2023-07-07 08:28:21 +0100

    fix for video not autoplaying on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,16 @@ export default function Home() {
         <div className="absolute top-0 left-0 w-full h-screen overflow-hidden">
           <video
             className="!min-w-full !min-h-full h-0 max-w-none object-cover"
+            playsInline
             src="./video.mp4"
             muted
             autoPlay
             loop
           ></video>
-          <div className="h-screen w-screen absolute top-0 left-0 z-10 opacity-70" style={{backgroundColor:"rgb(34, 84, 112)"}}></div>
+          <div
+            className="h-screen w-screen absolute top-0 left-0 z-10 opacity-70"
+            style={{ backgroundColor: "rgb(34, 84, 112)" }}
+          ></div>
         </div>
         <div className="z-20">
           <h1 className="text-5xl md:text-6xl lg:text-8xl">LONDONANDO</h1>
