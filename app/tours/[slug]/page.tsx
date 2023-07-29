bdadeda00e85fb5d75125dfe5812bb7bdcc8202a
Angelo Ilardi <ange448@gmail.com>
2023-07-29 08:51:57 +0100

    added carousel

--- app/tours/[slug]/page.tsx
+++ app/tours/[slug]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { RichText } from "@graphcms/rich-text-react-renderer";
 import Image from "next/image";
 import { Carousel } from "flowbite-react";
@@ -43,35 +45,53 @@ async function getTour(slug: string) {
 
 export default async function Tour({ params }: { params: { slug: string } }) {
   const tourData = await getTour(params.slug);
-  console.log(tourData);
   return (
     <main className="prose w-full py-10 px-5 mx-auto mt-6">
       <h1 className="text-3xl font-bold mb-5 text-center mt-6">
         {tourData.title}
       </h1>
+      <Carousel className="w-full">
+        {tourData.pictures.length !== 0 &&
+          tourData.pictures.map(
+            (picture: { url: string; alt: string; id: string }) => {
+              return (
+                <img
+                  key={picture.id}
+                  src={picture.url}
+                  alt={picture.alt}
+                //   width={400}
+                //   height={300}
+                  className="mb-5"
+                />
+              );
+            }
+          )}
+      </Carousel>
 
-          {tourData.pictures.length !== 0 &&
-              tourData.pictures.map((picture: { url: string,  alt: string, id: string; }) => {
-                  return (
-                      <Image
-                        key={picture.id}
-                      src={picture.url}
-                      alt={picture.alt}
-                      width={400}
-                      height={300}
-                      className="mb-5"
-                    />
-                  );
-})}
+      {/* {tourData.pictures.length !== 0 &&
+        tourData.pictures.map(
+          (picture: { url: string; alt: string; id: string }) => {
+            return (
+              <Image
+                key={picture.id}
+                src={picture.url}
+                alt={picture.alt}
+                width={400}
+                height={300}
+                className="mb-5"
+              />
+            );
+          }
+        )} */}
 
-      {tourData.content && (
+      {/* {tourData.content && (
         <RichText
           content={tourData?.content?.json}
           renderers={{
             p: ({ children }) => <p className="mb-5">{children}</p>,
           }}
         />
-      )}
+      )} */}
     </main>
   );
 }
--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,53 +1,78 @@
-
+"use client";
 interface Tour {
-  title: string,
-  slug: string,
-  content: {
-    html: string
-  }
-  id: string
-  
+  title: string;
+  slug: string;
+  id: string;
+  pictures?: [
+    {
+      url: string;
+      alt: string;
+      id: string;
+    }
+  ];
 }
-
+import { Card } from "flowbite-react";
 
 async function getTours() {
-  const response = await fetch('https://api-eu-west-2.hygraph.com/v2/cljyrgzcu0poz01uk2vye20j2/master', {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query: `
+  const response = await fetch(
+    "https://api-eu-west-2.hygraph.com/v2/cljyrgzcu0poz01uk2vye20j2/master",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
         query Tours {
             pages {
                 slug
                 title
-                    id
-                }                  
+                id
+                }   
+                pages {
+    pictures(first: 1) {
+      alt
+      url
+    }
+  }         
             }
         `,
-    }),
-  });
+      }),
+    }
+  );
   const json = await response.json();
 
   return json.data.pages;
-
-};
+}
 
 export default async function Tours() {
   const tours = await getTours();
-console.log(tours);
   return (
     <main className="prose w-full py-10 px-5 mx-auto mt-5">
       <h1 className="text-3xl font-bold mt-6">SCOPRI TUTTI I TOUR</h1>
       {tours.map((tour: Tour) => {
         return (
-            <div key={tour.id}>
-            <h2 className="text-2xl font-bold">
-              <a href={`/tours/${tour.slug}`}>{tour.title}</a>{" "}
-            </h2>
-            {/* <div>{tour.content.html}</div> */}
-          </div>
+          <Card
+            key={tour.id}
+            className="max-w-lg"
+            // imgAlt={tour.pictures[0].alt}
+            // imgSrc={tour.pictures[0].url}
+          >
+            <div className="p-4">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <a href={`/tours/${tour.slug}`}>{tour.title}</a>{" "}
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </div>
+          </Card>
+          //   <div key={tour.id}>
+          //   <h2 className="text-2xl font-bold">
+          //     <a href={`/tours/${tour.slug}`}>{tour.title}</a>{" "}
+          //   </h2>
+          // </div>
         );
       })}
     </main>
