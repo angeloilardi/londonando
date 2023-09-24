762e554e88960425ac5c78aec08dc6699e3afd38
Angelo Ilardi <ange448@gmail.com>
2023-09-24 20:03:45 +0100

    added go back button

--- app/tours/[slug]/page.tsx
+++ app/tours/[slug]/page.tsx
@@ -4,6 +4,9 @@ import { RichText } from "@graphcms/rich-text-react-renderer";
 import Image from "next/image";
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import { Carousel } from "flowbite-react";
+import Link from "next/link";
+import { BiArrowBack } from "react-icons/bi";
+
 
 async function getTour(slug: string) {
   const res = await fetch(process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT!, {
@@ -46,13 +49,18 @@ export default async function Tour({ params }: { params: { slug: string } }) {
   const tourData = await getTour(params.slug);
   return (
     <main className="prose w-full py-10 px-5 mx-auto mt-6">
+        <Link href="/tours">
+          <p className="text-xs">
+            <BiArrowBack className="inline-block mr-2"/>
+            Torna indietro
+          </p>
+        </Link>
       <h1 className="text-3xl font-bold mb-5 text-center mt-6">
         {tourData.title}
       </h1>
-
       <div className="w-90 max-w-xl my-16 mx-auto aspect-[3/2]">
         <Carousel>
-          {tourData.pictures.length !== 0 ?
+          {tourData.pictures.length !== 0 ? (
             tourData.pictures.map(
               (picture: { url: string; alt: string; id: string }) => {
                 return (
@@ -67,12 +75,12 @@ export default async function Tour({ params }: { params: { slug: string } }) {
                 );
               }
             )
-            : <img
-            alt="..."
-            src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-          />
-          }
-          
+          ) : (
+            <img
+              alt="..."
+              src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+            />
+          )}
         </Carousel>
       </div>
 
