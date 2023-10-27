10efd84aceec0913b23ef9979e4c9aa234a05187
Angelo Ilardi <ange448@gmail.com>
2023-10-27 21:14:11 +0100

    adjusted layout

--- app/servizi/page.tsx
+++ app/servizi/page.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 
 export default function Servizi() {
   return (
-    <div className="bg-[url(https://images.unsplash.com/photo-1527007622069-3a0241e1cd8c?auto=format&fit=crop&q=80&w=2874&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover">
+    <div className="bg-[url(https://images.unsplash.com/photo-1527007622069-3a0241e1cd8c?auto=format&fit=crop&q=80&w=2874&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover flex h-screen items-center justify-center">
       {/* <Image src="https://images.unsplash.com/photo-1527007622069-3a0241e1cd8c?auto=format&fit=crop&q=80&w=2874&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width={500} height={500} alt=""></Image> */}
-      <div className="flex flex-col md:flex-row items-center justify-center h-screen gap-8">
+      <div className="flex flex-col md:flex-row items-center justify-center h-screen gap-12 w-[85%]">
         <Card className="md:max-w-[40%]" href="/servizi/airport">
           <h1 className="mb-6 text-xl">TRANSFER PRIVATO AEROPORTO HOTEL</h1>
         </Card>
