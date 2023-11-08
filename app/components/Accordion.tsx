2ebd1b8c01606e9c7607a95dc90c8a785f4bd889
Angelo Ilardi <ange448@gmail.com>
2023-11-08 21:45:50 +0000

    fixed font

--- app/components/Accordion.tsx
+++ app/components/Accordion.tsx
@@ -7,7 +7,7 @@ import { UrlObject } from "url";
 function ButtonInfo(props: { url: string | UrlObject; }) {
     return (
       <Link href={props.url}>
-        <button className="flex items-center gap-3 dark:border-white border-black border p-2 rounded">
+        <button className="flex items-center gap-3 dark:border-white border-black border p-2 rounded mt-4">
           Richiedi info
           <BsArrowRightCircleFill />
         </button>
@@ -17,9 +17,9 @@ function ButtonInfo(props: { url: string | UrlObject; }) {
 
 export default function Component() {
   return (
-    <Accordion className="bg-white">
+    <Accordion collapseAll className="bg-white">
       <Accordion.Panel>
-        <Accordion.Title className="text-4xl">
+        <Accordion.Title className="text-2xl md:text-4xl">
           TRANSFER PRIVATO AEROPORTO HOTEL
         </Accordion.Title>
         <Accordion.Content>
@@ -29,16 +29,16 @@ export default function Component() {
             aeroporto (Stansted, Luton, Gatwick, Heathrow e London City) e a
             qualsiasi ora (h24).
           </p>
-          <ButtonInfo url="/servizi/airport" />
+          <ButtonInfo url="/servizi/airport"/>
         </Accordion.Content>
       </Accordion.Panel>
 
       <Accordion.Panel>
-        <Accordion.Title className="text-4xl">
+        <Accordion.Title className="text-2xl md:text-4xl">
           TRANSFER PRIVATO ALTRE DESTINAZIONI
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2  text-xl">
+          <p className="mb-2 text-xl">
             Trasferimenti su mezzo privato per escursioni, passeggiate, visite
             fuori Londra con o senza guida/ accompagnatore. L'autista vi
             accompagnerà e sarà a vostra completa disposizione per l'intera
