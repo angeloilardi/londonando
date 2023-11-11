fc6c29ff55e003c72832b4a5bc492f6f02d96c54
Angelo Ilardi <ange448@gmail.com>
2023-11-11 10:19:44 +0000

    separated widgrt from thepage

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -1,15 +1,11 @@
-import Script from "next/script";
+
+import ReviewsWidget from "../components/ReviewsWidget";
 
 export default function AboutMe() {
     return (
       <>
-        <Script
-          src="https://static.elfsight.com/platform/platform.js"
-          data-use-service-core
-          defer
-        />
-        <div className="p-20">
-          <div className="elfsight-app-fb710219-8601-424c-a299-7a8b954c1451 p-20 bg-black" />
+        <div className="pt-20">
+          <ReviewsWidget />
         </div>
       </>
     );
