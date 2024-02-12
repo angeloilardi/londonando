7c719b83cf8d22dced6678e09a735ce45ea99454
Angelo Ilardi <ange448@gmail.com>
2024-02-12 20:54:47 +0000

    fix for Missing Suspense boundary with useSearchParams

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -5,20 +5,29 @@ import FormBooking from "../components/FormBooking";
 import { useSearchParams } from "next/navigation";
 
 
-
+function GetParams() {
+   const searchParams = useSearchParams();
+  const tour = searchParams.get("tour");
+  const attendants = searchParams.get("num");
+  return (
+    <><h2 className="text-3xl font-bold">{tour}</h2>
+      <p className="text-lg">{attendants}</p></>
+  )
+  
+}
 
 export default function Booking() {
-  const searchParams = useSearchParams();
-  const tour = searchParams.get("tour") ?? null;
-  const attendants = searchParams.get("num") ?? null;
+
 
   return (
     <div className="pt-24 flex flex-col items-center">
       <div className="flex flex-col gap-4 max-w-2xl text-dodger_blue-400 my-6 mx-4 justify-center">
         <h1 className="text-2xl">La tua richiesta:</h1>
-          <h2 className="text-3xl font-bold">{tour}</h2>
-          <p className="text-lg">{attendants}</p>
+        <Suspense>
+          <GetParams />
+        </Suspense>
       </div>
+
       <FormBooking />
     </div>
   );
