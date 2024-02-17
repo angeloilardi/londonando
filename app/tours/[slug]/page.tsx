65aff589200ecf80efc5f23ec5f22fbb8dec82ef
Angelo Ilardi <ange448@gmail.com>
2024-02-17 23:06:06 +0000

    added submit button

--- app/tours/[slug]/page.tsx
+++ app/tours/[slug]/page.tsx
@@ -124,7 +124,7 @@ export default  async function Tour({ params }: { params: { slug: string } }) {
             name="current-route"
           />
           <Button
-            // type="submit"
+            type="submit"
             className="w-full group flex h-min items-center justify-center p-0.5 text-center font-medium relative focus:z-10 focus:outline-none text-white border border-transparent enabled:hover:bg-cyan-800 focus:ring-cyan-300 dark:bg-cyan-600 dark:enabled:hover:bg-cyan-700 dark:focus:ring-cyan-800 rounded-lg focus:ring-2 bg-cobalt_blue my-4"
           >
             {" "}
