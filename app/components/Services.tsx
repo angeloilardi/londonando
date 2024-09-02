7a149ff952d5d4d864fce50ece9740548d6194fb
Angelo Ilardi <ange448@gmail.com>
2024-09-02 18:29:43 +0100

    height fix

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -59,12 +59,12 @@ export default async function Services() {
   return (
     <div
       id="servizi"
-      className="bg-[url(https://images.unsplash.com/photo-1530458738063-22ed42fa27c9?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center items-center flex flex-col justify-center py-10 snap-start min-h-screen"
+      className="bg-[url(https://images.unsplash.com/photo-1530458738063-22ed42fa27c9?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center items-center min-h-screen justify-center py-10 snap-start overflow-auto"
     >
       <h3 className="text-center text-3xl text-white">SERVIZI</h3>
       <Accordion
         collapseAll
-        className="bg-primary grow-0 max-w-[80%] my-6 md:w-[60%] hover:text-primary dark:bg-gray-900 opacity-90"
+        className="bg-primary grow-0 max-w-[80%] my-6 md:w-[60%] hover:text-primary dark:bg-gray-900 opacity-90 mx-auto"
       >
         {services.map((service: Service) => {
           return (
