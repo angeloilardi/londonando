10ecb87eb807937f0e1dbe92930393728ed6a3b4
Angelo Ilardi <ange448@gmail.com>
2024-09-11 22:51:22 +0100

    added privacy policy

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,40 +1,50 @@
-// import { RichText } from "@graphcms/rich-text-react-renderer";
+import { RichText } from "@graphcms/rich-text-react-renderer";
 
-// export default async function PrivacyPolicy() {
-//   async function getprivacyPolicy() {
-//     const res = await fetch(process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT!, {
-//       // cache: 'no-store',
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         query: `
-//         query Privacy {
-//   docs {
-//     content{html}
-//   }
-// }`,
-//       }),
-//     });
-//     const data = await res.json();
-//     console.log(data.data.docs.content);
-//     return data;
-//   }
+export default async function PrivacyPolicy() {
+  async function getprivacyPolicy() {
+    const res = await fetch(process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT!, {
+      // cache: 'no-store',
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+       query Privacy {
+  doc (where: {id: "cm0seyekmk05a07mis0oxnp4o"}) {
+    content{
+      json
+    }
+  }
+}`,
+      }),
+    });
+    const data = await res.json();
+console.log(data.data.doc)
+    return data.data.doc;
+  }
 
-//   const PrivacyPolicy = await getprivacyPolicy();
-//     return (
-//       <div>
-//         <RichText
-//           content={PrivacyPolicy?.content}
-//           renderers={{
-//             p: ({ children }) => (
-//               <p className="mb-5 text-accent/60 dark:text-indigo-50">
-//                 {children}
-//               </p>
-//             ),
-//           }}
-//         />
-//       </div>
-//     );
-// }
+  const PrivacyPolicy = await getprivacyPolicy();
+    return (
+      <div className="p-10 bg-background-light dark:bg-accent">
+        <RichText
+          content={PrivacyPolicy?.content?.json}
+          renderers={{
+            p: ({ children }) => (
+              <p className="mb-5 text-accent/60 dark:text-indigo-50">
+                {children}
+              </p>
+            ),
+            h1: ({ children }) => <h1 className="text-3xl">{children}</h1>,
+            li: ({ children }) => (
+              <li className="list-disc">{children}</li>
+            ),
+            ul: ({ children }) => (
+              <ul className="py-3">{children}</ul>
+            ),
+            
+          }}
+        />
+      </div>
+    );
+}
