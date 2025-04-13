fa5d850ba2a5a2ccb2ec55c5dbe1d1ba1114ce6d
Angelo Ilardi <ange448@gmail.com>
2025-04-13 09:24:15 +0100

    changed language

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${rubik.className} scroll-smooth`}>
+    <html lang="it" className={`${rubik.className} scroll-smooth`}>
       <meta charSet="utf-8"></meta>
       <body className="overflow-x-hidden relative h-screen flex flex-col">
         <Navbar />
